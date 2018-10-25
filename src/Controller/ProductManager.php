92563bd2b195e9a175599705b6dd73aff171903e
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-25 19:11:33 +1100

    Skeleton of controller

--- src/Controller/ProductManager.php
+++ src/Controller/ProductManager.php
@@ -21,7 +21,38 @@ class ProductManager {
      */
     public function indexAction() : string
     {
-        
         return $this->simpleView->render(VIEWS_DIR . '/index.phtml', []);
+    }
+
+    /**
+     * Create a product
+     */
+    public function createAction() : string
+    {
+        return $this->simpleView->render(VIEWS_DIR . '/create.phtml', []);
+    }
+
+    /**
+     * Edit a product
+     */
+    public function editAction() : string
+    {
+        return $this->simpleView->render(VIEWS_DIR . '/edit.phtml', []);
+    }
+
+    /**
+     * Product details page
+     */
+    public function detailsAction() : string
+    {
+        return $this->simpleView->render(VIEWS_DIR . '/details.phtml', []);
+    }
+
+    /**
+     * Upsert (insert or update) a Product
+     */
+    public function upsertAction() : string
+    {
+        header('Location : /');
     }
 }
