cbb3687283937ee6d6163b560f4412b432b3ea1d
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-25 23:26:44 +1100

    Simple test for deleteAction()

--- tests/ProductManagerTest.php
+++ tests/ProductManagerTest.php
@@ -5,7 +5,9 @@ use PHPUnit\Framework\TestCase;
 use ProductManager\Product;
 use ProductManager\Controller\ProductManager;
 
-
+/**
+ * New mocks are created for each test
+ */
 class ProductManagerTest extends TestCase {
     public function testIndexAction() {
         $productCollection = \Mockery::mock('MongoDB\Collection');
@@ -28,6 +30,24 @@ class ProductManagerTest extends TestCase {
         );
 
         $output = $productManager->indexAction();
+        $this->assertEquals('test', $output);
+    }
+
+    public function testCreateAction() {
+        $productCollection = \Mockery::mock('MongoDB\Collection');
+        $productFactory = \Mockery::mock('ProductManager\ProductFactory');
+        $uploadHelper = \Mockery::mock('ProductManager\UploadHelper');
+        $simpleView = \Mockery::mock('ProductManager\SimpleView');
+        $simpleView->shouldReceive('render')->once()->andReturn('test');
+
+        $productManager = new ProductManager(
+            $productCollection,
+            $productFactory,
+            $uploadHelper,
+            $simpleView
+        );
+
+        $output = $productManager->createAction();
         $this->assertEquals('test', $output);
     }
 }
