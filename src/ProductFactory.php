df7923318a8d7178deb2ba1bba58eaedf31a4599
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-25 19:05:58 +1100

    Skeleton of ProductFactory

---
+++ src/ProductFactory.php
@@ -0,0 +1,19 @@
+<?php
+class ProductFactory {
+    /**
+     * Create a Product from a MongoDB document
+     */
+    public function createFromObject(array $document) {
+
+    }
+
+    /**
+     * Create and return array of Products using documents from a MongoDB query result
+     *
+     * @param documents
+     * @return array
+     */
+    public function createFromArray(array $documents) : array {
+
+    }
+}
