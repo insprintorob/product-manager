6a0bb44cc5e6199bd652f322285749c92bf87058
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-25 23:43:33 +1100

    Test for postCreateAction

--- tests/ProductManagerTest.php
+++ tests/ProductManagerTest.php
@@ -86,4 +86,39 @@ class ProductManagerTest extends TestCase {
         $response = $productManager->postEditAction($request, $response);
         $this->assertNotNull($response);
     }
+
+    public function testPostCreateAction() {
+        $productCollection = \Mockery::mock('MongoDB\Collection');
+        $productFactory = \Mockery::mock('ProductManager\ProductFactory');
+        $uploadHelper = \Mockery::mock('ProductManager\UploadHelper');
+        $simpleView = \Mockery::mock('ProductManager\SimpleView');
+        $simpleView->shouldReceive('render')->once()->andReturn('test');
+        $response = \Mockery::mock('Slim\Http\Response');
+        $request = \Mockery::mock('Slim\Http\Request');
+
+        $request->shouldReceive('getUploadedFiles')->once()->andReturn([
+            'picture' => \Mockery::mock('Slim\Http\UploadedFile')
+        ]);
+
+        $uploadHelper->shouldReceive('moveUploadedFile')->once();
+
+        $request->shouldReceive('getParam')->once()->with('name')->andReturn('Tesla solar roof tile');
+        $request->shouldReceive('getParam')->once()->with('price')->andReturn('100.00');
+        $request->shouldReceive('getParam')->once()->with('description')->andReturn('Solar panels that look like normal roof tiles');
+
+        $productCollection->shouldReceive('insertOne')->once();
+
+        $response->shouldReceive('withStatus')->once()->with(302)->andReturn($response);
+        $response->shouldReceive('withHeader')->once()->andReturn($response);
+
+        $productManager = new ProductManager(
+            $productCollection,
+            $productFactory,
+            $uploadHelper,
+            $simpleView
+        );
+
+        $response = $productManager->postCreateAction($request, $response);
+        $this->assertNotNull($response);
+    }
 }
